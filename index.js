e1ebbfd3f6795cdb65fa631427b9a064c7b63822
David Fairfield <david.fairfield@gmail.com>
2018-06-27 15:19:42 -0800

    Refactor

--- index.js
+++ index.js
@@ -18,48 +18,35 @@ module.exports = {
             var filePath = './';
             copy_pdf(filePath);
 
-            function copy_pdf(filePath)
-            {
+            function copy_pdf(filePath){
                 //Return file list
-                fs.readdir(filePath, function(err, files)
-                {
-                    if (err)
-                    {
+                fs.readdir(filePath, function(err, files){
+                    if (err){
                         console.warn(err)
-                    }
-                    else
-                    {
+                    }else{
                         //Traverse files in file list.  
-                        files.forEach(function(filename)
-                        {
+                        files.forEach(function(filename){
+
                             var filedir = path.join(filePath, filename);
                             var filePath_new = path.join('./_book', filePath);
                             //Return fs.Stats object  
-                            fs.stat(filedir, function(eror, stats)
-                            {
-                                if (eror)
-                                {
+                            fs.stat(filedir, function(err, stats){
+                                if (err){
                                     console.warn('Get file stats failed');
-                                }
-                                else
-                                {
+                                }else{
                                     var isFile = stats.isFile();
                                     var isDir = stats.isDirectory();
-                                    if (isFile)
-                                    {
+                                    if (isFile){
                                         var extension = path.extname(filename);
-                                        if (".pdf" == extension.toLowerCase())
-                                        {
-                                            var cmd = 'mkdir -p ' + filePath_new;
-                                            cmd = cmd + ';\\cp -f "' + filedir + '" "' + filePath_new + '"';
-                                            //console.log(cmd);
-                                            exec(cmd, function(error, stdout, stderr)
-                                            {
-                                                if (error)
-                                                {
-                                                    console.log(error);
-                                                    return;
-                                                }
+                                        if (".pdf" == extension.toLowerCase()){
+                                            // var cmd = 'mkdir -p ' + filePath_new;
+                                            // cmd = cmd + ';\\cp -f "' + filedir + '" "' + filePath_new + '"';
+                                            // //console.log(cmd);
+                                            // exec(cmd, function(error, stdout, stderr){
+                                            //     if (error){
+                                            //         console.log(error);
+                                            //         return;
+                                            //     }
                                             })
                                         }
                                     }
